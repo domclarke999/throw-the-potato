683cb5a6ba604fb9be62d2cc160f4a8fe1299a48
domclarke999 <domclarke999@gmail.com>
2026-01-15 19:09:48 +0000

    Update client.js

--- public/client.js
+++ public/client.js
@@ -14,19 +14,15 @@ let audioUnlocked = false;
 
 const audio = new Audio("incoming.wav");
 
-// Unlock audio for iOS Safari
+// Unlock audio for iOS
 function unlockAudio() {
   if (!audioUnlocked) {
-    audio.play().then(() => {
-      audio.pause();
-      audio.currentTime = 0;
-      audioUnlocked = true;
-    }).catch(() => {});
+    audio.play().then(() => { audio.pause(); audio.currentTime = 0; audioUnlocked = true; }).catch(()=>{});
   }
 }
 
-document.body.addEventListener("click", unlockAudio, { once: true });
-document.body.addEventListener("touchstart", unlockAudio, { once: true });
+document.body.addEventListener("click", unlockAudio, { once:true });
+document.body.addEventListener("touchstart", unlockAudio, { once:true });
 
 const protocol = location.protocol === "https:" ? "wss" : "ws";
 const ws = new WebSocket(`${protocol}://${location.host}`);
@@ -34,7 +30,7 @@ const ws = new WebSocket(`${protocol}://${location.host}`);
 let playerDivs = {};
 
 function vibrate() {
-  navigator.vibrate?.([200, 100, 200]);
+  navigator.vibrate?.([200,100,200]);
 }
 
 function updatePlayers(players) {
@@ -45,26 +41,36 @@ function updatePlayers(players) {
     div.classList.add("player");
     div.innerText = pid;
     div.dataset.pid = pid;
-    div.style.position = "absolute";
     div.style.left = `${idx * 80}px`;
-    div.style.top = "70px";
     playerDivs[pid] = div;
     playersContainer.appendChild(div);
   });
 }
 
-// Animate potato to target player
-function movePotatoTo(pid) {
-  const target = playerDivs[pid];
+// Animate potato fly
+function movePotatoTo(newHolderId) {
+  const target = playerDivs[newHolderId];
   if (!target) return;
 
   const containerRect = playersContainer.getBoundingClientRect();
   const targetRect = target.getBoundingClientRect();
 
-  const x = targetRect.left - containerRect.left + targetRect.width / 2 - potato.offsetWidth / 2;
-  const y = targetRect.top - containerRect.top + targetRect.height / 2 - potato.offsetHeight / 2;
+  // Start at last holder
+  if (lastHolder) {
+    const lastRect = playerDivs[lastHolder].getBoundingClientRect();
+    const startX = lastRect.left - containerRect.left + lastRect.width/2 - potato.offsetWidth/2;
+    const startY = lastRect.top - containerRect.top + lastRect.height/2 - potato.offsetHeight/2;
+    potato.style.transform = `translate(${startX}px, ${startY}px)`;
+  }
 
-  potato.style.transform = `translate(${x}px, ${y}px)`;
+  // Target coordinates
+  const x = targetRect.left - containerRect.left + targetRect.width/2 - potato.offsetWidth/2;
+  const y = targetRect.top - containerRect.top + targetRect.height/2 - potato.offsetHeight/2;
+
+  // Animate
+  requestAnimationFrame(() => {
+    potato.style.transform = `translate(${x}px, ${y}px)`;
+  });
 }
 
 ws.onmessage = e => {
@@ -92,29 +98,30 @@ ws.onmessage = e => {
     timerText.innerText = `⏱ ${Math.ceil(msg.timeRemaining / 1000)}s`;
   }
 
-  // Animate potato flying to new holder
+  // Fly potato
   if (msg.potatoHolder && lastHolder !== msg.potatoHolder) {
     potatoInFlight = true;
     movePotatoTo(msg.potatoHolder);
 
-    // Wait for animation to finish (~1s)
     setTimeout(() => {
       potatoInFlight = false;
+
       if (msg.potatoHolder === myId) {
         gameText.innerText = "🔥 YOU HAVE THE POTATO!";
         throwBtn.disabled = false;
-        if (audioUnlocked) audio.play().catch(() => {});
+        if (audioUnlocked) audio.play().catch(()=>{});
         vibrate();
       } else {
         gameText.innerText = "🥔 Someone else has the potato";
         throwBtn.disabled = true;
       }
+
       lastHolder = msg.potatoHolder;
-    }, 1000); // match CSS transition
+    }, 1000);
   }
 };
 
-// Throw potato
+// Throw button
 throwBtn.onclick = () => {
   if (!potatoInFlight) {
     potatoInFlight = true;
